90aea94e33bdbdc0da2f2371813b2f189017c8c9
andre-cotrim <andrecotrim2005@gmail.com>
2024-07-31 10:28:53 +0100

    finish binary and decimal converters

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # binary-conversor
+Binary and Decimal Converter
+(Octal and Hexadcimal coming soon)
+Css file made by Chat-GPT 4
--- converter.js
+++ converter.js
@@ -1,13 +1,23 @@
 function converter(){
-    var value = document.getElementById('resposta')
-var res = document.getElementById('resultado')
-    if(isNaN(value.value)){
-        res.innerHTML ='Escreva um número'
-    }else{
-        //tostring
-        var tamanho = value.length
-        res.innerHTML =tamanho
-        /* for(i=0;i<value.lengthl;i++)
-        res.innerHTML =  */
+    var value = document.getElementById('resposta').value
+    var res = document.getElementById('resultado')
+    var opcao = document.getElementById('opcoes').value 
+    if(isNaN(value) || value == ''){
+        res.innerHTML ='Escreva um número válido'
+    }else if (opcao=="decimal->binary"){
+        var resultado = ""
+        var num = parseInt(value,10)
+        while (num>0){
+            if (num%2==0){
+                resultado+='0'
+            }else{
+                resultado+='1'
+            }
+            num = Math.floor(num/2)
+        }  
+        res.innerHTML=resultado.split('').reverse().join('')
+    }else if (opcao=="binary->decimal"){
+        var num = parseInt(value,2)
+        res.innerHTML = num.toString()
     }
 }
